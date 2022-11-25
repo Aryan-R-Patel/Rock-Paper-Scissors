be1444e2950930b36a8cf0485462165e0900a5e8
Aryan-R-Patel <aryanpatel31206@gmail.com>
2022-11-25 16:50:30 +0530

    added comments

--- game.py
+++ game.py
@@ -6,9 +6,11 @@ print("Welcome to the game of Rock Paper Scissors.")
 print("Type Rock/Paper/Scissors, and press Enter. ")
 print("===========================================")
 
+# taking inputs
 player = input("You: ").lower()
 bot = random.randint(1, 3)
 
+# assigning value to the bot according to the random integer generated
 if (bot==1):
     bot = "rock"
 elif (bot==2):
@@ -18,6 +20,7 @@ elif (bot==3):
 
 print("Bot:",bot)
 
+# displaying the result
 if(player==bot):
     print("It is a tie!")
 elif((player=="rock" and bot=="scissors") or (player=="paper" and bot=="rock") or (player=="scissors" and bot=="paper")):
