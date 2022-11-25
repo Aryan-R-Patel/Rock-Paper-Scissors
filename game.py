14b9a9aa83dcec8b74b312983b84d7c916f10bfe
Aryan-R-Patel <aryanpatel31206@gmail.com>
2022-11-25 16:48:39 +0530

    Initial Commit

--- game.py
+++ game.py
@@ -3,25 +3,24 @@ import random
 # welcome statement
 print("===========================================")
 print("Welcome to the game of Rock Paper Scissors.")
-print("To start playing, simply enter Rock or Paper")
-print("or Scissors and then press Enter.          ")
+print("Type Rock/Paper/Scissors, and press Enter. ")
 print("===========================================")
 
-player = input("You: ").capitalize()
+player = input("You: ").lower()
 bot = random.randint(1, 3)
 
 if (bot==1):
-    bot = "Rock"
+    bot = "rock"
 elif (bot==2):
-    bot = "Paper"
+    bot = "paper"
 elif (bot==3):
-    bot = "Scissors"
+    bot = "scissors"
 
 print("Bot:",bot)
 
 if(player==bot):
     print("It is a tie!")
-elif((player=="Rock" and bot=="Scissors") or (player=="Paper" and bot=="Rock") or (player=="Scissors" and bot=="Paper")):
+elif((player=="rock" and bot=="scissors") or (player=="paper" and bot=="rock") or (player=="scissors" and bot=="paper")):
     print("You Win!")
 else:
     print("You Lose!")
